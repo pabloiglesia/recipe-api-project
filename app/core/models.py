debc9b19ebc31a6b889262b48638a2d404d55b9b
pabloiglesia <pabloiglesia@outlook.com>
2023-06-14 17:45:57 +0200

    Corrección Modelo de usuarios

--- app/core/models.py
+++ app/core/models.py
@@ -24,7 +24,7 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password):
         """Create and return a new superuser."""
-        user = self.create_user(email,password)
+        user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
